e0cea8e2a8d6f779538ff101fc3a45f8777070fb
Paul Tingle <paul.tingle@7lb.co.uk>
2014-05-04 10:15:15 +0100

    updates after_restart hook to echo rollbar vars

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,7 +2,7 @@ run "echo 'exec gem list'"
 run "cd #{current_path} && bundle exec gem list"
 run "echo 'non-exec gem list'"
 run "gem list"
-ACCESS_TOKEN=36c978e891854ff6abafb3033ee4914b
-LOCAL_USERNAME=`whoami`
+#ACCESS_TOKEN=36c978e891854ff6abafb3033ee4914b
+#LOCAL_USERNAME=`whoami`
 #run "echo $ACCESS_TOKEN >> /home/deploy/echoes.txt && echo $ENV >> /home/deploy/echoes.txt && echo $COMMIT >> /home/deploy/echoes.txt && echo $LOCAL_USERNAME >> /home/deploy/echoes.txt"
 run "echo $ACCESS_TOKEN >> /home/deploy/echoes.txt"
